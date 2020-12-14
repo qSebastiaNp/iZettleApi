53fbebe3626cf835f3648cf4dfee041eba23ae58
Sebastian <qSebastiaNp@users.noreply.github.com>
2020-12-14 03:36:24 +0100

    Update VariantChangeHistoryBuilder.php

--- src/Client/Inventory/VariantChangeHistoryBuilder.php
+++ src/Client/Inventory/VariantChangeHistoryBuilder.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace LauLamanApps\IzettleApi\Client\Inventory;
 
-use LauLamanApps\IzettleApi\API\Inventory\History;
+use LauLamanApps\IzettleApi\API\Inventory\VariantChangeHistory;
 
 final class VariantChangeHistoryBuilder implements VariantChangeHistoryBuilderInterface
 {
-    public function buildFromJson(string $json): History
+    public function buildFromJson(string $json): VariantChangeHistory
     {
         // TODO: Implement buildFromJson() method.
     }
