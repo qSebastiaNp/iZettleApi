0c8e7c0bd8ce627000dc106f6b0230c4e47c3edc
Sebastian <qSebastiaNp@users.noreply.github.com>
2020-12-15 00:39:55 +0100

    Update ProductBalanceBuilder.php
    Make the class usable

--- src/Client/Inventory/ProductBalanceBuilder.php
+++ src/Client/Inventory/ProductBalanceBuilder.php
@@ -5,11 +5,31 @@ declare(strict_types=1);
 namespace LauLamanApps\IzettleApi\Client\Inventory;
 
 use LauLamanApps\IzettleApi\API\Inventory\ProductBalance;
+use LauLamanApps\IzettleApi\API\Inventory\LocationBalance;
+use Ramsey\Uuid\Uuid;
+use LauLamanApps\IzettleApi\API\Inventory\Location\TypeEnum;
 
 final class ProductBalanceBuilder implements ProductBalanceBuilderInterface
 {
     public function buildFromJson(string $json): ProductBalance
     {
-        // TODO: Implement buildFromJson() method.
+        $data = json_decode($json, true);
+        
+        $variants = [];
+        foreach ($data['variants'] as $variant) {
+            $variants []= new LocationBalance(
+                    Uuid::fromString($variant['locationUuid']),
+                    TypeEnum::get($variant['locationType']),
+                    Uuid::fromString($variant['productUuid']),
+                    Uuid::fromString($variant['variantUuid']),
+                    (int) $variant['balance']
+                );
+        }
+        
+        return new ProductBalance(
+            Uuid::fromString($data['locationUuid']),
+            $variants
+        );
     }
+    
 }
